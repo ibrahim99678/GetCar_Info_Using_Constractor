3a5cdf38d79f80c7f310bbe4d2333b96c9515bc4
Mohammad Ibrahim khalil <101477328+ibrahim99678@users.noreply.github.com>
2024-05-28 08:57:19 +0600

    Add files via upload

---
+++ Ex_01StaticModifier/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01StaticModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // WE ARE GOING TO MAKE A APP TO KNOW HOW MANY CARS WAS IN A RACE?
+
+            //Static : Modifier to decliar a static member, which belongs to the class itself
+            // rather than to any specific object
+
+            Car car1 = new Car("Mustang");                 //object has ownership of it so, that is the static 
+            Car car2 = new Car("Chevy");
+            Car car3 = new Car("LamBergini");           //Just add another object
+
+
+            Console.WriteLine(Car.numberOfCars);        // Assign the main class.
+            Car.StartRace();                            // startRace ststic method we are using here through parentases
+
+
+
+ 
+            Console.ReadKey();
+        }
+        class Car                                       // class is own this properties
+        {
+            String make;
+            public static int numberOfCars;             // we just make here static and its own this by using static
+
+            public Car(String make)
+            {
+                this.make = make;
+                numberOfCars++;
+            }
+            public static void StartRace()              //using a static modifier to a method 
+
+            {
+                Console.WriteLine("The Race has Begun!");
+            }                           
+
+        }
+    }
+}
---
+++ GetCar_Info_UsingConstractor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetCar_Info_UsingConstractor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            car car1 = new car("Ford", "Mustang", 2021, "red");                 // create object / assign a identifier 
+            car car2 = new car("Chevy", "Carvetta", 2020, "Blue");
+            
+            car1.drive();                                                         // here we using the drive method
+            car2.drive();
+
+
+            Console.ReadKey();
+        }
+        class car                        //Make a class for a car to know model, year and color
+        {
+            string make;
+            string model;
+            int year;
+            string color;
+
+            public car(string make,  string model, int year, string color)                  //make a constractor same name as class and using arguments
+            {
+                this.make = make;
+                this.model = model;
+                this.year = year;
+                this.color = color;
+            }
+            public void drive()                         //assign a method 
+            {
+                Console.WriteLine("You Drive the " + make + " " + model);
+            }
+        }
+
+    }
+}
